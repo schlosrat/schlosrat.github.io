5168b47f225b555e728dd638bc9fde81decadb4e
Falki-git <72734856+Falki-git@users.noreply.github.com>
2023-11-17 11:54:17 +0100

    Add a few comments for module methods

--- src_stripped/KSP/Sim/impl/PartComponentModule.cs
+++ src_stripped/KSP/Sim/impl/PartComponentModule.cs
@@ -58,15 +58,31 @@ namespace KSP.Sim.impl
     [MethodImpl(MethodImplOptions.NoInlining)]
     public virtual void ThermalUpdate(double deltaTime) => throw null;
 
+    /// <summary>
+    /// This triggers when the Flight scene is first loaded. When loading a game it triggers for in-flight vessels also.
+    /// </summary>
     [MethodImpl(MethodImplOptions.NoInlining)]
     public virtual void OnStart(double universalTime) => throw null;
 
+    /// <summary>
+    /// This starts triggering when vessel is first placed in Flight. Does not trigger while still in OAB.
+    /// Keeps triggering in every scene once it's in Flight.
+    /// OnUpdate is called every frame.
     [MethodImpl(MethodImplOptions.NoInlining)]
     public virtual void OnUpdate(double universalTime, double deltaUniversalTime) => throw null;
 
+    /// <summary>
+    /// This triggers when the part is destroyed, when leaving the Flight scene, when exiting the game.
+    /// It... also triggers when Flight scene is loaded? (why? maybe it's triggered when leaving the OAB scene)
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
     [MethodImpl(MethodImplOptions.NoInlining)]
     public virtual void OnShutdown() => throw null;
 
+    /// <summary>
+    /// It appears this doesn't trigger at all?
+    /// </summary>
     [MethodImpl(MethodImplOptions.NoInlining)]
     public virtual void OnFinalizeCreation(double universalTime) => throw null;
 
